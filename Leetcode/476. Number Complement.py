fd536cd6b66c8d28c8b2e02d4a3244fb4578a180
Vishnu <vishnusingh1057@gmail.com>
2021-12-27 11:37:28 +0530

    27-December-2021

---
+++ Leetcode/476. Number Complement.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        a = math.log2(num)
+        a = math.floor(a)
+        a+=1
+        a = math.pow(2,a)-1
+        a = int(a-num)
+        return a
+        
